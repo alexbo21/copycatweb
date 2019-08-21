01c93426530f2e306faa96cb52a4aad1ab048918
alexbo21 <53921135+alexbo21@users.noreply.github.com>
2019-08-22 00:37:14 +0100

    Add files via upload

---
+++ ver.js
@@ -0,0 +1,8 @@
+function makeSmall(){
+  var x = document.getElementById("downloads");
+  if (x.style.display === "none"){
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  };
+};
