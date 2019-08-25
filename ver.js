6efb7c4bffb03fae732bfcbcb3bac5c6d102e8cd
alexbo21 <53921135+alexbo21@users.noreply.github.com>
2019-08-25 22:47:18 +0100

    Delete ver.js

--- ver.js
+++
@@ -1,8 +0,0 @@
-function makeSmall(){
-  var x = document.getElementById("downloads");
-  if (x.style.display === "none"){
-    x.style.display = "block";
-  } else {
-    x.style.display = "none";
-  };
-};
